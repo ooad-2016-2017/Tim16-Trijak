6ec9f646daeeceaa5c616afd20cb80680d5b0c0d
Emina Modric <emodric1@etf.unsa.ba>
2017-05-21 23:29:23 +0200

    Dodana klasa TipKorisnika

---
+++ Projekat/SmartGreenhouse/SmartGreenhouse/Model/TipKorisnika.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.Model
+{
+    public class TipKorisnika
+    {
+        private String naziv;
+        private int ID;
+
+        public TipKorisnika(String naziv, int iD)
+        {
+            this.naziv = naziv;
+            ID = iD;
+        }
+
+        public String Naziv
+        {
+            get
+            {
+                return naziv;
+            }
+
+            set
+            {
+                naziv = value;
+            }
+        }
+
+        public int ID1
+        {
+            get
+            {
+                return ID;
+            }
+
+            set
+            {
+                ID = value;
+            }
+        }
+    }
+}
