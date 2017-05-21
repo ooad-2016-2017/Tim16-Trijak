9631e79b0682c8606a1598b66c088f8019d130a3
Emina Modric <emodric1@etf.unsa.ba>
2017-05-21 23:37:35 +0200

    Dodana klasa PreuzimanjeInformacija

---
+++ Projekat/SmartGreenhouse/SmartGreenhouse/Model/PreuzimanjeInformacija.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.Model
+{
+    public class PreuzimanjeInformacija
+    {
+        private double vlaznostZraka;
+        private double vlaznostZemljista;
+        private double nivoVode;
+        private double osvijetljenost;
+        private double temperatura;
+
+        public double VlaznostZraka
+        {
+            get
+            {
+                return vlaznostZraka;
+            }
+
+            set
+            {
+                vlaznostZraka = value;
+            }
+        }
+
+        public double VlaznostZemljista
+        {
+            get
+            {
+                return vlaznostZemljista;
+            }
+
+            set
+            {
+                vlaznostZemljista = value;
+            }
+        }
+
+        public double NivoVode
+        {
+            get
+            {
+                return nivoVode;
+            }
+
+            set
+            {
+                nivoVode = value;
+            }
+        }
+
+        public double Osvijetljenost
+        {
+            get
+            {
+                return osvijetljenost;
+            }
+
+            set
+            {
+                osvijetljenost = value;
+            }
+        }
+
+        public double Temperatura
+        {
+            get
+            {
+                return temperatura;
+            }
+
+            set
+            {
+                temperatura = value;
+            }
+        }
+        public void pokreniSistemZaNavodnjavanje() { }
+    }
+}
