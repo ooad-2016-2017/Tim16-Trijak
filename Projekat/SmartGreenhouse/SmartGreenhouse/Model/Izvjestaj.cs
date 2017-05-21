511284f809010ae5ed48e7eece04977126102891
Emina Modric <emodric1@etf.unsa.ba>
2017-05-22 00:03:09 +0200

    Dodana klasa Izvjestaj

---
+++ Projekat/SmartGreenhouse/SmartGreenhouse/Model/Izvjestaj.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.Model
+{
+    public class Izvjestaj
+    {
+        private DateTime datumIzvjestaja;
+        private Sadnica[] vrstaSadnice;
+        private TipIzvjestaja tipIzvjestaja;
+
+        public Izvjestaj(DateTime datumIzvjestaja, Sadnica[] vrstaSadnice, TipIzvjestaja tipIzvjestaja)
+        {
+            this.datumIzvjestaja = datumIzvjestaja;
+            this.vrstaSadnice = vrstaSadnice;
+            this.tipIzvjestaja = tipIzvjestaja;
+        }
+
+        public Izvjestaj() { }
+
+        public DateTime DatumIzvjestaja
+        {
+            get
+            {
+                return datumIzvjestaja;
+            }
+
+            set
+            {
+                datumIzvjestaja = value;
+            }
+        }
+
+        internal Sadnica[] VrstaSadnice
+        {
+            get
+            {
+                return vrstaSadnice;
+            }
+
+            set
+            {
+                vrstaSadnice = value;
+            }
+        }
+
+        public TipIzvjestaja TipIzvjestaja
+        {
+            get
+            {
+                return tipIzvjestaja;
+            }
+
+            set
+            {
+                tipIzvjestaja = value;
+            }
+        }
+        
+        
+    }
+}
