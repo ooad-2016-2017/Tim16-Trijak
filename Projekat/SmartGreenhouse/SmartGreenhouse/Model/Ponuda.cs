b183f7526366bbb9445fb565ef3fb57eea96a04e
Emina Modric <emodric1@etf.unsa.ba>
2017-05-22 00:55:29 +0200

    Dodana klasa Ponuda

---
+++ Projekat/SmartGreenhouse/SmartGreenhouse/Model/Ponuda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.Model
+{
+    public class Ponuda
+    {
+        private int brojSadnica;
+        private List<Sadnica> sadniceUPonudi;
+
+        public Ponuda(List<Sadnica> sadniceUPonudi)
+        {
+            this.sadniceUPonudi = sadniceUPonudi;
+            this.brojSadnica = sadniceUPonudi.Count;
+        }
+
+        public Ponuda() { }
+
+        public void dodajSadnicu(Sadnica sadnica)
+        {
+            this.sadniceUPonudi.Add(sadnica);
+        }
+
+        public void obrisiSadnicu(Sadnica sadnica)
+        {
+            this.sadniceUPonudi.Remove(sadnica);
+        }
+
+        public int vratiRedniBrojSadnice(Sadnica sadnica)
+        {
+            return sadniceUPonudi.IndexOf(sadnica);
+        }
+        
+    }
+}
--- Projekat/SmartGreenhouse/SmartGreenhouse/Model/Statistika.cs
+++ Projekat/SmartGreenhouse/SmartGreenhouse/Model/Statistika.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmartGreenhouse.Model
 {
-    class Statistika
+    public class Statistika
     {
         private Sadnica vrstaSadnice;
         private DateTime datumIzvjestaja;
