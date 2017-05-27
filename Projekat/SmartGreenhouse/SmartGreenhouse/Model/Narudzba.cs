1e3f6a053d6b6faef3466147136138334547c9a7
npulo1 <npulo1@etf.unsa.ba>
2017-05-27 13:28:28 +0200

    dodano Narudzba.cs

---
+++ Projekat/SmartGreenhouse/SmartGreenhouse/Model/Narudzba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGreenhouse.Model
+{
+    class Narudzba
+    {
+        private DateTime datumNarudzbe;
+        static int redniBrojNarudzbe = 0;
+
+        public Narudzba(DateTime datumNarudzbe)
+        {
+            this.datumNarudzbe=datimNarudzbe;
+            redniBrojNarudzbe = redniBrojNarudzbe + 1;
+
+        }
+        public static int sljedeciBrojNaudzbe();
+
+        public void dodajIzabarnuSadnicu(Sadnica stavka){}
+        public bool validirajNarudzbu(Sadnica stvka) {
+            if (this.stavka.Prodato - this.stavka.Kolicina < 0)
+                return false;
+            else
+                return true;
+        }
+        public bool rezervirajNarudzbu(Sadnica stavka)
+        {
+            this.stavka.Prodato = this.stavka.Prodato - this.Kolicina;
+        }
+
+       
+    }
