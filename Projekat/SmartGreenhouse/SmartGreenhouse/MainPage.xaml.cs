640063d68bac5f8c9db411a78ed05a9a2b88ac33
npulo1 <npulo1@etf.unsa.ba>
2017-06-11 16:56:45 +0200

    ispravka za azure

--- Projekat/SmartGreenhouse/SmartGreenhouse/MainPage.xaml.cs
+++ Projekat/SmartGreenhouse/SmartGreenhouse/MainPage.xaml.cs
@@ -20,7 +20,7 @@ using SmartGreenhouse.Azure;
 
 namespace SmartGreenhouse
 {
-    /// <summary>
+    
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
