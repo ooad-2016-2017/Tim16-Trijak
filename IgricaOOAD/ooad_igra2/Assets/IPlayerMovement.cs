7e0c82569e9280d819228b505ab22049330efcca
npulo1 <npulo1@etf.unsa.ba>
2017-06-12 14:46:55 +0200

    Prepravke igre

---
+++ IgricaOOAD/ooad_igra2/Assets/IPlayerMovement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IPlayerMovement : MonoBehaviour
+{
+
+    public abstract void Movement(float forwardSpeed, float HorizSpeed, float[] Lanes, int currLane);
+
+}
---
+++ IgricaOOAD/ooad_igra2/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : IPlayerMovement
+{
+
+
+    public override void Movement(float forwardSpeed, float HorizSpeed, float[] Lanes, int currLane)
+    {
+
+        var rigid = transform.GetComponent<Rigidbody>();
+
+        rigid.velocity = forwardSpeed * transform.forward * Time.fixedDeltaTime;
+
+        Vector3 pos = rigid.position;
+        pos.x = Mathf.Lerp(pos.x, Lanes[currLane], HorizSpeed * Time.fixedDeltaTime);
+
+        rigid.position = pos;
+    }
+
+
+
+}
